3efc9282bdcdac2488893aae3ced192e006f96df
Peter Eames <peterdeames@gmail.com>
2022-11-16 20:49:35 +0000

    remove unittest

--- sonarqube/audit.py
+++ sonarqube/audit.py
@@ -14,6 +14,7 @@ logging.basicConfig(
 def __check_version(url, token):
     urltopost = url + "/api/server/version"
     current_version = requests.get(urltopost, auth=(token, ""), timeout=30)
+    print(current_version)
     return current_version
 
 
@@ -49,13 +50,16 @@ def get_version(url, token):
     current_version = __check_version(url, token)
     urltopost = url + "/api/system/upgrades"
     upgradecheck = requests.get(urltopost, auth=(token, ""), timeout=30)
+    print(upgradecheck)
     json_object = json.loads(upgradecheck.text)
     try:
         lts = json_object["latestLTS"]
     except KeyError:
         lts = '9999999'
+    print(lts)
     if current_version.ok:
         tmp_version = current_version.text[0:3]
+        print(tmp_version)
         if len(json_object["upgrades"]) > 0:
             upgrades_available = True
         if float(lts) > float(tmp_version) and float(lts) < 1000:
--- tests/test_audit.py
+++ tests/test_audit.py
@@ -1,6 +1,6 @@
 """ audit unit tests """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from nose.tools import assert_equals
 from sonarqube import audit
@@ -11,6 +11,14 @@ def test_ping(mock_get):
     mock_get.return_value.text = 'pong'
     response = audit.ping('URL', 'TOKEN')
     assert_equals(response, 'pong')
+# {"health":"GREEN","causes":[]}
+
+
+""" @patch('sonarqube.audit.requests.get')
+def test_get_version(mock_get):
+    mock_get.side_effect = [9.7,'{"upgrades":[],"latestLTS":"8.9","updateCenterRefresh":"2022-11-15T21:31:29+0000"}']
+    response = audit.get_version('URL', 'TOKEN')
+    assert_equals(response, '8.9') """
 
 
 if __name__ == "__main__":
